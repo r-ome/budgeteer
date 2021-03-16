7c94ac4646830922456fea583e22371c21fc4b16
muming <jerome.arceo.agapay@gmail.com>
2021-03-16 14:56:34 +0800

    feat: added body parser

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
-import express from "express";
-import { seedDatabase } from "./database/seed";
+import express from 'express';
+import userRoutes from './routes/users';
+import bodyParser from 'body-parser';
+
+import { seedDatabase } from './database/seed';
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/users', userRoutes);
 
 app.get('/', (req, res) => {
-    seedDatabase();
-    res.send('hello world!');
+  seedDatabase();
+  res.send('hello world!');
 });
 
-app.listen(3000, () => console.log("server running!"));
+app.listen(3000, () => console.log('server running!'));
